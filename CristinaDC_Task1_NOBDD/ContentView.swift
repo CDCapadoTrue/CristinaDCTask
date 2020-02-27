ec611adc7b9de6fc549ebf2341bf761425bb8997
CristinaCapado <cristinadcapado@gmail.com>
2020-02-27 02:22:41 +0000

    Initial Commit

---
+++ CristinaDC_Task1_NOBDD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CristinaDC_Task1_NOBDD
+//
+//  Created by user167360 on 2/27/20.
+//  Copyright © 2020 user167360. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
